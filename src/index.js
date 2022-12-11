14558c3c60c0144d2b7906bd02c534f81853cbfa
Tasha852209 <112940506+Tasha852209@users.noreply.github.com>
2022-12-11 13:11:47 +0100

    Updated All

--- src/index.js
+++ src/index.js
@@ -100,3 +100,5 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Kyiv");
